8066abdd4c3e8eba644b1e227c6c5498034e7498
AmandaMatar <amandamatar@gmail.com>
2023-03-07 10:42:19 -0300

    Commits do projeto

---
+++ src/main/java/one/digitalinnovation/diodesafiooadroesprojetospring/DioDesafioOadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.diodesafiooadroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioOadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioOadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/diodesafiooadroesprojetospring/DioDesafioOadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.diodesafiooadroesprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioOadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
